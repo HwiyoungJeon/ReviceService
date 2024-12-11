b88810b4a2620623f0122c532c15c2467f6dbd3d
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-12-11 10:20:05 +0900

    Initial commit

---
+++ src/main/java/com/example/reviceservice/ReviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reviceservice/ReviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
