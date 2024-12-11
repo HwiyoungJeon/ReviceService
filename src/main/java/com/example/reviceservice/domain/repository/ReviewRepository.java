04dd1222048706741857e5858ba198491198853e
Hwiyoung Jeon <wjsgnldud@naver.com>
2024-12-11 17:26:17 +0900

    Meber,Review Repository 추가 커밋작업
    Ref: #8

---
+++ src/main/java/com/example/reviceservice/domain/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.example.reviceservice.domain.repository;
+
+import com.example.reviceservice.domain.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
